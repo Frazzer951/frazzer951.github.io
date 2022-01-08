fa1e0121ad365ac00568bd72a257c7784e9d04a5
Luke Eltiste <luke343279@gmail.com>
2022-01-08 12:19:25 -0800

    About Me Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
-import { Container, Paper, Typography } from "@mui/material";
+import { Box, Container, Stack, Paper, Typography } from "@mui/material";
 import "../style.css";
+import me from "../resources/me.jpg";
 
 function About() {
   return (
@@ -11,54 +12,46 @@ function About() {
           padding: "5px 10px",
         }}
       >
-        <Typography variant="h1" component="div">
+        <Typography variant="h3" component="div" align="center">
           About Me
         </Typography>
-        <Typography variant="p" component="div">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet
-          ligula eget mi tempus, fermentum gravida orci luctus. Integer ac nisi
-          condimentum, maximus nulla ac, mattis libero. Duis sed vestibulum
-          lacus. Aenean libero nisi, lacinia ut nisl eget, faucibus lobortis
-          est. Fusce non turpis tortor. Morbi eget orci vel arcu volutpat
-          pellentesque ac ac diam. Morbi eget cursus sem. Donec laoreet nisi
-          arcu, vel lobortis arcu volutpat eget. Etiam non maximus diam. Nulla
-          luctus aliquet nisl. Proin quis eros justo. Curabitur ultrices commodo
-          feugiat. In hac habitasse platea dictumst. Ut pharetra maximus urna in
-          auctor. Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. Nullam maximus, tortor nec faucibus
-          feugiat, libero dui sodales tortor, pharetra elementum mauris lectus a
-          ante. Vestibulum hendrerit sed diam at faucibus. Mauris sagittis
-          libero elit, at efficitur risus tempus vel. Fusce at sem ac augue
-          feugiat pharetra a at velit. Mauris orci orci, eleifend ac justo vel,
-          lacinia sagittis est. Etiam non ligula massa. Cras vitae metus
-          maximus, commodo mauris vel, dapibus sapien. Praesent ac fringilla
-          sem. Phasellus a luctus metus, non fermentum velit. Proin velit urna,
-          accumsan sit amet velit nec, egestas viverra nisl. Integer id magna eu
-          massa volutpat luctus quis ultrices arcu. Sed malesuada lacinia
-          malesuada. Integer fringilla aliquet leo ut tincidunt. Nam quis dolor
-          eget massa feugiat eleifend. In nec volutpat ante. Fusce quis quam ex.
-          Nam id elit nec lectus sodales faucibus. Mauris lacinia auctor elit,
-          vitae maximus lorem eleifend in. Sed convallis metus nulla, eu auctor
-          nisi suscipit ac. Maecenas libero neque, auctor id fringilla nec,
-          egestas id augue. Sed tincidunt felis cursus diam rutrum porttitor.
-          Morbi suscipit ante non magna facilisis vehicula. Orci varius natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam
-          fringilla justo at justo accumsan, vitae dictum enim pellentesque. Sed
-          eu sem sit amet nisl cursus mattis. Morbi ut aliquet mauris.
-          Pellentesque habitant morbi tristique senectus et netus et malesuada
-          fames ac turpis egestas. Ut justo ex, pellentesque non lacinia non,
-          pulvinar at nulla. Integer sodales, enim ac porta eleifend, augue
-          ligula bibendum nulla, vitae sodales ante orci et nisi. Maecenas quis
-          sagittis enim. Quisque tempus justo ac mi gravida, ac efficitur massa
-          fermentum. Phasellus eget mattis eros. Quisque neque sem, tincidunt id
-          quam eu, imperdiet accumsan turpis. Duis et fringilla est, non
-          tincidunt lorem. Praesent id blandit dolor. Maecenas elementum ac
-          sapien quis condimentum. Quisque in viverra leo. Fusce pulvinar, diam
-          a laoreet cursus, nisl magna congue metus, non mollis nibh enim quis
-          eros. Nullam mollis aliquet mollis. Morbi commodo efficitur leo, sit
-          amet rhoncus nisi laoreet at. Integer efficitur rhoncus egestas.
-          Integer faucibus semper fermentum.
-        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+        >
+          <Box sx={{ width: "45%" }}>
+            <Typography
+              variant="body1"
+              align="left"
+              paragraph
+              sx={{ fontSize: 20 }}
+            >
+              I am a Computer Science Major currently attending California State
+              University Fullerton, for my third year. I am currently living in
+              Anaheim, California, and interning for the company Industrial
+              Logic Systems.
+            </Typography>
+            <Typography
+              variant="body1"
+              align="left"
+              paragraph
+              sx={{ fontSize: 20 }}
+            >
+              I work on many programming projects, ranging from web development
+              to C++. I use these projects to learn new technologies and improve
+              my skills.
+            </Typography>
+          </Box>
+
+          <img
+            src={me}
+            alt="Me"
+            height="500px"
+            style={{ borderRadius: "35px" }}
+          />
+        </Stack>
       </Paper>
     </Container>
   );
