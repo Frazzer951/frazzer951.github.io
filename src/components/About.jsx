0a45b31bcca7c720ebff0e387e5e5e9827aa0374
Luke Eltiste <luke343279@gmail.com>
2022-10-04 14:46:49 -0700

    update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,12 +20,14 @@ export default function About() {
         <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
           <Box sx={{ width: "45%" }}>
             <Typography variant="body1" align="left" paragraph sx={{ fontSize: 20 }}>
-              I am a Computer Science Major currently attending California State University Fullerton, for my third year. I am
-              currently living in Anaheim, California, and interning for the company Industrial Logic Systems.
+              I am a Computer Science Major currently attending California State University Fullerton, for my fourth year. I am
+              currently living in Anaheim, California.
             </Typography>
             <Typography variant="body1" align="left" paragraph sx={{ fontSize: 20 }}>
-              I work on many programming projects, ranging from web development to C++. I use these projects to learn new
-              technologies and improve my skills.
+              I work on many programming projects, to develop and learn new skills. My current favorite language to use is
+              Rust, because its low level and fast like C++, but has many high level features that other similar languages
+              don't have. Project's I've worked on range from computer renders like Ray Tracing and Ray Marching to low-level
+              control of 6-DOF motion platforms.
             </Typography>
           </Box>
 
