7c64ba34786bed37dda44188606b0aba123aa39d
Luke Eltiste <luke343279@gmail.com>
2022-10-04 14:07:25 -0700

    update packages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from "./App";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const theme = createTheme({
   palette: {
     mode: "dark",
