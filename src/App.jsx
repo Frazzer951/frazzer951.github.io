26737384be735920360761a6db048e6ecba22176
Luke Eltiste <luke343279@gmail.com>
2022-10-04 14:11:21 -0700

    switch to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Header from "./Header";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+
 import "./style.css";
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
+import { theme } from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
+
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
